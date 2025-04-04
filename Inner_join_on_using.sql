e32697ba11ed7ef6653381abfb9cd28fa1b1354f
Aziz Khan <manruption@gmail.com>
2025-04-04 09:32:05 -0400

    Code for USING & ON parameters

--- Inner_join_on_using.sql
+++ Inner_join_on_using.sql
@@ -20,9 +20,29 @@ INNER JOIN economies AS e
 -- Match on code field using table aliases
 ON e.code = c.code;
 
---Finding countries with Official languages
+--Finding countries with Official languages                                                       
 SELECT c.name AS country, l.name AS language, official
 FROM countries AS c
 INNER JOIN languages AS l
 -- Match using the code column
 USING(code)
+
+-- Inner join of countries AS c (left) with populations AS p (right), on code. Select name and fertility_rate
+Select name and fertility_rate
+SELECT c.name, p.fertility_rate
+FROM countries as c
+-- Inner join countries and populations, aliased, on code
+INNER JOIN populations as p
+ON c.code = p.country_code;
+
+-- Join tables to return the country name, year, fertility rate, and unemployment rate in a single result from the countries, populations and economies tables.
+SELECT name, e.year, fertility_rate, e.unemployment_rate
+FROM countries AS c
+INNER JOIN populations AS p
+ON c.code = p.country_code
+-- Join to economies (as e)
+INNER JOIN economies AS e 
+-- Match on country code
+ON e.code = c.code AND e.code = p.country_code;
+-- Add an additional joining condition such that you are also joining on year
+	AND p.year = e.year;
