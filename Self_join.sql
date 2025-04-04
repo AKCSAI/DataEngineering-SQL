a8e2a22a8821180e6150d8044fcf71d7cfae3d8d
Aziz Khan <manruption@gmail.com>
2025-04-04 12:18:27 -0400

    Code for using SELF JOIN

---
+++ Self_join.sql
@@ -0,0 +1,11 @@
+--Find out how much the populations for each country changed from 2010 to 2015. 
+SELECT 
+	p1.country_code, 
+    p1.size AS size2010, 
+    p2.size AS size2015
+FROM populations AS p1
+INNER JOIN populations AS p2
+ON p1.country_code = p2.country_code
+WHERE p1.year = 2010
+-- Filter such that p1.year is always five years before p2.year
+    AND p1.year = p2.year -5 -- Filter such that p2.year is always five years after p1.year
