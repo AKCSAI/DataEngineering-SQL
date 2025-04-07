fec4c77c8b68e94104880bb5b5a7b00a07369fe1
Aziz Khan <manruption@gmail.com>
2025-04-07 09:07:12 -0400

    Code to ALTER COLUMN

--- Corruption_Investigation/Alter_column.sql
+++ Corruption_Investigation/Alter_column.sql
@@ -12,3 +12,9 @@ ALTER TABLE professors
 ALTER COLUMN firstname
 TYPE VARCHAR (64);
 
+-- Convert the values in firstname to a max. of 16 characters
+ALTER TABLE professors 
+ALTER COLUMN firstname 
+TYPE varchar(16)
+USING SUBSTRING(firstname FROM 1 For 16)
+
