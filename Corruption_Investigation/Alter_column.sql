b86a182beead3a4dfe3ff71c0253509c52f88b09
Aziz Khan <manruption@gmail.com>
2025-04-08 08:36:39 -0400

    Code to ALTER COLUMN

--- Corruption_Investigation/Alter_column.sql
+++ Corruption_Investigation/Alter_column.sql
@@ -18,3 +18,10 @@ ALTER COLUMN firstname
 TYPE varchar(16)
 USING SUBSTRING(firstname FROM 1 For 16)
 
+-- Drop the firstname column
+ALTER TABLE affiliations
+DROP COLUMN firstname;
+
+-- Drop the lastname column
+ALTER TABLE affiliations
+DROP COLUMN lastname;
