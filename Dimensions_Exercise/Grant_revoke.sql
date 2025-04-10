8a5a797c5cad48bd185d0fd68ce79eb5f546acc3
Aziz Khan <manruption@gmail.com>
2025-04-10 13:39:58 -0400

    Code to REVOKE or GRANT permissions

--- Dimensions_Exercise/Grant_revoke.sql
+++ Dimensions_Exercise/Grant_revoke.sql
@@ -3,3 +3,15 @@ REVOKE INSERT, UPDATE ON  long_reviews FROM PUBLIC;
 
 -- Grant the editor update and insert privileges 
 GRANT INSERT, UPDATE ON long_reviews TO editor; 
+
+-- Grant data_scientist update and insert privileges
+GRANT UPDATE, INSERT ON long_reviews TO data_scientist;
+
+-- Give Marta's role a password
+GRANT ROLE marta WITH password 's3cur3p@ssw0rd';
+
+-- Add Marta to the data scientist group
+GRANT data_scientist TO Marta;
+
+-- Remove Marta from the data scientist group
+REVOKE data_scientist FROM Marta;
